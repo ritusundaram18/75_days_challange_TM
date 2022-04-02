222f80c5ca795d307f499c85c46a4edefc3663fb
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-04-03 00:44:15 +0530

    Time: 7 ms (50.93%), Space: 46.9 MB (9.12%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,35 +1,33 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        //HM Ka use karungi
+        //unsorted ayyay hai to HM ka use karungi
         //put karungi
-        //rem = target - num
-        //index == i- ontinue
+        //num = nums[i], rem = targe-num
+        // fir check karungi  rem HM m hai ya nahi
+        //agar HM m hai aur uska indec i  itna hi hai to continu karungi
         
         HashMap<Integer,Integer> map = new HashMap<>();
         
-        for(int i =0; i<nums.length;i++)
+        for(int i =0; i< nums.length;i++)
         {
             map.put(nums[i],i);
         }
-        
-        
-        //search
+        //searching
         for(int i=0;i< nums.length;i++)
         {
-            int num= nums[i];
-            int rem= target-num;
+            int num = nums[i];
+            int rem = target -num;
             
             if(map.containsKey(rem))
             {
                 int index = map.get(rem);
                 if(index == i)
                     continue;
+                
                 return new int[]{i,index};
             }
-        }    
-            return new int[]{};
-        
-        
+        }
+        return new int[]{};
     }
 }
