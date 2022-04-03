9e9f9e7580db6204e16bdc9199399f7b35865f9b
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-04-04 01:01:54 +0530

    Time: 11 ms (60.28%), Space: 46.4 MB (23.94%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,23 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
     
-        int start = 0;
+       int start =0;
         int end = 0;
         int max = 0;
         int len = s.length();
-        Set<Character> st =new HashSet<>();
-        while(end < len)
+        
+        Set<Character> st = new HashSet<>();
+        while(end<len)
         {
             char ch = s.charAt(end);
-                while(st.contains(ch))
-                {
-                    st.remove(s.charAt(start));
-                    start++;
-                }
+            while(st.contains(ch))
+            {
+                st.remove(s.charAt(start));
+                start++;
+            }
             st.add(ch);
             end++;
-            max = Math.max(max, (end-start));
+            max = Math.max(max,(end-start));
         }
         return max;
+        
     }
 }
+//yeh basically sliding window ka problem hai
+//toh start and end variable lenge he lenge
+//max = lenge max subrting ckeck karnae k liye
+//set ka use karnge, agar character phele se he present hai , toh remove kr denge  start index se
+//aur nahi  hai to set me add kr denge , end++ kr denge aur max check kr lenge
+//lasr me max return kr denge
+//ACCQUIRE & RELEASE
