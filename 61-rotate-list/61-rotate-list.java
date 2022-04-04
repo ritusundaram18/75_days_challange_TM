263ddda45ae007d00357f297f1fb75a90cf3b70a
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-04-04 11:30:12 +0530

    Time: 1 ms (76.29%), Space: 42.8 MB (66.78%) - LeetHub

--- 61-rotate-list/61-rotate-list.java
+++ 61-rotate-list/61-rotate-list.java
@@ -10,27 +10,24 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        //1.check the age cases
         if(head == null || head.next == null || k == 0)
             return head;
-        //2compute the length
-        ListNode cur = head;
-        int len = 1;
+        ListNode cur  = head;
+        int len =1;
+        //find length;
         while(cur.next != null)
         {
             len++;
             cur = cur.next;
         }
-        //3 go till that node
-        cur.next = head;
-            // k = k%len;
-            k=len-k%len;
-        while(k-- >0)
+        cur.next= head; //last ka node se null hata diye
+        //compute k
+        k=len-k%len;
+        while(k-->0)
             cur = cur.next;
-        //4. make the node head and break the connection
-        head= cur.next;
-        cur.next = null;
+        head =cur.next;
+        cur.next=null;
         
-    return head;
+        return head;
     }
 }
