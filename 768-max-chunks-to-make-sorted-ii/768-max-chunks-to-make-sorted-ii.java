a3bcb29f618ed578ca26e543df5f7da6ac7385e5
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-04-05 12:46:26 +0530

    Time: 1 ms (86.55%), Space: 45.3 MB (38.91%) - LeetHub

---
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        //generate right min
+        int[] rightMin = new int[arr.length+1];
+        
+        rightMin[arr.length] = Integer.MAX_VALUE;
+        for(int i= arr.length-1; i>=0;i--)
+            rightMin[i] = Math.min(rightMin[i+1], arr[i]);
+        
+        //step 2. iterate an array and manage leftrt max as well as count chunks
+        int leftMax = Integer.MIN_VALUE;
+        int count = 0;
+        
+        for(int i = 0; i< arr.length;i++)
+        {
+            leftMax = Math.max(leftMax,arr[i]);
+            if(leftMax <= rightMin[i+1])
+                count++;
+        }
+        return count;
+        
+    }
+}
