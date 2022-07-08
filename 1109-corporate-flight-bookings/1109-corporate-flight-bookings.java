4a7f20cc55bdd7ab0380467dd2692fe580b4c6a3
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-07-08 10:43:25 +0530

    Time: 2298 ms (5.08%), Space: 75.6 MB (68.78%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -1,15 +1,37 @@
+//Brute force
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int ans[] = new int[n];
-        for(int[] a:bookings)
-        {
-            ans[a[0]-1] +=a[2];
-            if(a[1]<n) ans[a[1]] -=a[2];
+ 
+        int res[] = new int [n];
+        int start;
+        int end;
+        int seats;
+        for(int j=0;j<bookings.length;j++){
+            for(int i=bookings[j][0];i<=bookings[j][1];i++)
+            {
+                // int start = bookings[i][0];
+                // int end = bookings[i][1];
+                  seats = bookings[j][2];
+                res[i-1] +=seats;
+            }
         }
-        for(int i=1; i<n; i++)
-        {
-            ans[i] +=ans[i-1]; 
-        }
-        return ans;
+        return res;
     }
 }
+
+
+// class Solution {
+//     public int[] corpFlightBookings(int[][] bookings, int n) {
+//         int ans[] = new int[n];
+//         for(int[] a:bookings)
+//         {
+//             ans[a[0]-1] +=a[2];
+//             if(a[1]<n) ans[a[1]] -=a[2];
+//         }
+//         for(int i=1; i<n; i++)
+//         {
+//             ans[i] +=ans[i-1]; 
+//         }
+//         return ans;
+//     }
+// }
