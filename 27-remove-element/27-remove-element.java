3d81497ad3e32e4c6cca38f06f5da111f213c34e
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-07-08 09:55:12 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (43.48%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -1,19 +1,37 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
+     public int removeElement(int[] nums, int val) {
+    
+         int c =0;
+         for(int i=0;i< nums.length; i++)
+         {
+             if(nums[i] != val)
+             {
+                 nums[c++] = nums[i];
+             }
+         }
+         return c;
      
-        int maxvalid = 0;
-        int n = nums.length;
-        
-        for(int i=0; i<n; i++){
-            if(nums[i] != val){
-                swap(nums,i,maxvalid++);
-            }
-        }
-        return maxvalid;
-    }
-    void swap(int[] arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+     }
 }
+
+
+//method 2 ........................
+// class Solution {
+//     public int removeElement(int[] nums, int val) {
+     
+//         int maxvalid = 0;
+//         int n = nums.length;
+        
+//         for(int i=0; i<n; i++){
+//             if(nums[i] != val){
+//                 swap(nums,i,maxvalid++);
+//             }
+//         }
+//         return maxvalid;
+//     }
+//     void swap(int[] arr, int i, int j){
+//         int temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//     }
+// }
