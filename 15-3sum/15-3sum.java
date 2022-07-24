9055e2f50b97e68bc3ca822c0925a920306bb96d
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-07-24 09:19:02 +0530

    Time: 23 ms (96.72%), Space: 60.3 MB (29.80%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,37 +1,48 @@
 class Solution {
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> threeSum(int[] nums) {
        Arrays.sort(nums);
-        
-        // List<List<Integer>> res = new ArrayList<>();
-        // we can use both ArrayList or linked list , percentage varies
-          List<List<Integer>> res = new LinkedList<>();
-        for(int i = 0 ; i < nums.length-2; i++)
+    
+        for(int i=0;i<nums.length;i++)
         {
-            if(i == 0 ||(i > 0 && nums[i] != nums[i-1]))
+            if(i==0 || nums[i]!=nums[i-1])
             {
-               int lo = i+1 ,hi = nums.length-1, sum = 0 - nums[i];
-                
-                while(lo < hi)
-                {
-                    if(nums[lo]+nums[hi] == sum)
-                    {
-                        res.add(Arrays.asList(nums[i],nums[lo], nums[hi]));
-                        while(lo < hi && nums[lo] == nums[lo+1])
-                            lo++;
-                        while(lo < hi && nums[hi] == nums[hi-1])     hi--;
-                        
-                        lo++;
-                        hi--;
-                    }
-                    else if(nums[lo]+nums[hi] < sum)
-                        lo++;
-                    else
-                        hi--;
-                        
-                    }
-                }
+                twoSumSorted(i+1,nums.length-1,nums,0-nums[i]);
             }
-        return res;
         }
-            }
-// }
+        return res;      
+    }
+    void twoSumSorted(int i,int j, int[] nums, int target)
+    {
+    int a=nums[i-1];
+     while(i<j){
+        if(nums[i] + nums[j] > target)
+            j--;
+        
+        else if(nums[i] + nums[j] < target)
+            i++;
+        else{
+            List<Integer> list= new ArrayList<>();
+            list.add(a);
+            list.add(nums[i]);
+            list.add(nums[j]);
+            
+            res.add(list);
+            
+            while(i<j && nums[i] == nums[i+1]) i++;
+            while(i<j && nums[j]== nums[j-1]) j--;
+            
+            i++;
+            j--;
+        }  
+         
+         
+         /*
+         
+         
+         
+         */
+            
+        }
+    }
+}
