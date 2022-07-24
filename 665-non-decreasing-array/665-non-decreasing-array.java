4723acb48eb4f75f28088de652cfcf56e69a4d48
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-07-24 11:40:32 +0530

    Time: 2 ms (33.75%), Space: 53.5 MB (74.86%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
-        // boolean flag =false;
         int pos=-1;
-        for(int i=0;i<nums.length-1;i++)
-        {
-            if(nums[i]>nums[i+1])
+            for(int i=0;i<nums.length-1;i++)
             {
-                if(pos!=-1)
-                {    return false;}
-                      pos = i;
+                if(nums[i]>nums[i+1])
+                {
+                    if(pos != -1)   return false;
+                    
+                    pos=i;
+                }
             }
-        }
-        return pos == 0 ||pos ==-1 || pos ==nums.length-2||nums[pos] <= nums[pos+2] || nums[pos-1] <= nums[pos+1];
-               
+                return (pos==-1 || pos ==0 || pos ==nums.length-2 || nums[pos-1] <=nums[pos+1] 
+                       || nums[pos] <= nums[pos+2]);
     }
 }
