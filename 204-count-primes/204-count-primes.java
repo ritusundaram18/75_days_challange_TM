238baccdbb7262fbcd25125199a465d6d6506218
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-07-29 00:08:27 +0530

    Time: 201 ms (42.73%), Space: 68.3 MB (60.21%) - LeetHub

---
+++ 204-count-primes/204-count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+   boolean[] pr = new boolean[n+1];
+        for(int i = 0; i < pr.length; i++){
+            pr[i] = true;
+        }
+        
+        for(int i = 2; i*i <= n; i++){
+            
+            if(pr[i]){
+                for(int j = i*i; j <= n; j = j + i){
+                    pr[j] = false;
+                }
+            }   
+        }
+        int res = 0;
+        for(int i = 2 ; i < pr.length-1; i++){
+            if(pr[i]) res++;
+        }
+        return res;
+    }
+}
