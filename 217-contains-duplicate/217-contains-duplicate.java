16a01694be8769d7716371dc4ec548e4246130b6
ritusundaram18 <45529477+ritusundaram18@users.noreply.github.com>
2022-08-31 02:04:32 +0530

    Time: 18 ms (37.51%), Space: 71.6 MB (18.34%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -14,3 +14,18 @@ class Solution {
         
     }
 }
+/*
+public boolean containsDuplicate(int[] nums){
+HashMap<Integer,Integer> map=new HashMap<>();
+
+for(int i=0;i<nums.length;i++)
+{
+    if(map.containsKey(nums[i]))
+    {
+        return true;
+    }
+    map.put(nums[i],map.getOrDefault(nums[i],0)+1)
+}
+}
+
+*/
